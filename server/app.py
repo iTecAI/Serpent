f5741d964a10066d30afb5b9452f2f8eacccee9f
Dax Harris <matteovh@gmail.com>
2023-08-09 12:48:58 -0400

    download target setup

--- server/app.py
+++ server/app.py
@@ -11,9 +11,11 @@ import uuid
 
 load_dotenv()
 
+
 @get("/")
 async def get_root() -> str:
     return time.ctime()
+
 
 DB_PATH = os.getenv("SERPENT_DATABASE", "./db/db.json")
 if not os.path.exists(DB_PATH):
@@ -23,16 +25,20 @@ if not os.path.exists(DB_PATH):
     admin_user.admin = "forced"
     admin_user.dump(_tempdb.table("users"))
 
+if not os.path.exists(os.getenv("SERPENT_DOWNLOADS", "./media")):
+    os.makedirs(os.getenv("SERPENT_DOWNLOADS", "./media"), exist_ok=True)
+
 db = TinyDB(DB_PATH)
+
 
 def server_error_handler(request: Request, exc: Exception) -> Response:
     request.logger.exception("Encountered server error:\n")
-    return Response(
-        media_type=MediaType.TEXT,
-        content=str(exc),
-        status_code=500
-    )
+    return Response(media_type=MediaType.TEXT, content=str(exc), status_code=500)
 
-app = Litestar(route_handlers=[get_root, AuthController], state=State({"db": db}), dependencies={"db": Provide(depends_db), "log": Provide(depends_log)}, exception_handlers={
-    500: server_error_handler
-})
+
+app = Litestar(
+    route_handlers=[get_root, AuthController],
+    state=State({"db": db, "download_root": os.getenv("SERPENT_DOWNLOADS", "./media")}),
+    dependencies={"db": Provide(depends_db), "log": Provide(depends_log)},
+    exception_handlers={500: server_error_handler},
+)
